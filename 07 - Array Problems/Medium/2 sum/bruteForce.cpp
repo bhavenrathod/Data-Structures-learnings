fe71594265874351903868c46f6e5dc83f9035b1
Bhaven Rathod <rathodbhavendilip@gmail.com>
2024-03-17 23:56:31 +0530

    2 sum done

---
+++ 07 - Array Problems/Medium/2 sum/bruteForce.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string twoSum(int n, vector<int> &arr, int target)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[i] + arr[j] == target)
+                return "YES";
+        }
+    }
+    return "NO";
+}
+
+// TC is O(n*n)
+// SC is O(1)
+
+int main()
+{
+    int n = 5;
+    vector<int> arr = {2, 6, 5, 8, 11};
+    int target = 14;
+    string ans = twoSum(n, arr, target);
+    cout << "This is the answer for variant 1: " << ans << endl;
+    return 0;
+}
---
+++ 07 - Array Problems/Medium/2 sum/optimal.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string twoSum(int n, vector<int> &arr, int target)
+{
+    sort(arr.begin(), arr.end());
+    int left = 0, right = n - 1;
+    while (left < right)
+    {
+        int sum = arr[left] + arr[right];
+        if (sum == target)
+        {
+            return "YES";
+        }
+        else if (sum < target)
+            left++;
+        else
+            right--;
+    }
+    return "NO";
+}
+
+// TC is O(n) + (nlogn) where n is the size of the array
+// SC is O(1)
+
+int main()
+{
+    int n = 5;
+    vector<int> arr = {2, 6, 5, 8, 11};
+    int target = 14;
+    string ans = twoSum(n, arr, target);
+    cout << "This is the answer for variant 1: " << ans << endl;
+    return 0;
+}
