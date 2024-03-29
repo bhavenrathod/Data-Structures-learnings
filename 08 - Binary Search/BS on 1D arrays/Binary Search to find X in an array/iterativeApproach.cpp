8594c06b9a1ec856805696b8cbb7048c3de286b3
Bhaven Rathod <rathodbhavendilip@gmail.com>
2024-03-29 15:13:49 +0530

    Binary Search in a sorted array done

---
+++ 08 - Binary Search/BS on 1D arrays/Binary Search to find X in an array/iterativeApproach.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int search(vector<int> &arr, int n, int target)
+{
+    int low = 0;
+    int high = n - 1;
+    while (low <= high)
+    {
+        int mid = (low + high) / 2;
+        if (arr[mid] == target)
+            return mid;
+        else if (arr[mid] < target)
+            low = mid + 1;
+        else
+            high = mid - 1;
+    }
+    return -1;
+}
+
+// TC is O(log n)
+
+int main()
+{
+    vector<int> arr = {1, 2, 3, 4, 5, 6};
+    int n = arr.size();
+    int target = 2;
+    int answer = search(arr, n, target);
+
+    if (answer == -1)
+        cout << "The element is not present in the array";
+    else
+        cout << "The element is present at index " << answer;
+    return 0;
+}
---
+++ 08 - Binary Search/BS on 1D arrays/Binary Search to find X in an array/recursiveApproach.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int search(vector<int> &arr, int low, int high, int target)
+{
+    if (low > high)
+        return -1; // Target not found
+
+    int mid = (low + high) / 2;
+
+    if (arr[mid] == target)
+        return mid; // Target found at index mid
+    else if (arr[mid] < target)
+        return search(arr, mid + 1, high, target); // Search in the right half
+    else
+        return search(arr, low, mid - 1, target); // Search in the left half
+}
+
+// TC is O(log n)
+
+int main()
+{
+    vector<int> arr = {1, 2, 3, 4, 5, 6};
+    int n = arr.size();
+    int low = 0;
+    int high = n - 1;
+    int target = 4;
+    int ans = search(arr, low, high, target);
+    if (ans == -1)
+        cout << "The element is not present in the array";
+    else
+        cout << "The element is present at index " << ans;
+    return 0;
+}
