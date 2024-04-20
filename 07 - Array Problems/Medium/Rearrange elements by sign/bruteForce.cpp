79ca9b8ce8164ebf6698f3037d819f32a73fdbcc
Bhaven Rathod <rathodbhavendilip@gmail.com>
2024-04-20 12:06:50 +0530

    rearrange elements done

---
+++ 07 - Array Problems/Medium/Rearrange elements by sign/bruteForce.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> rearrange(vector<int> arr, int n)
+{
+    vector<int> pos;
+    vector<int> neg;
+
+    // Segregate the array into positives and negatives.
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] > 0)
+            pos.push_back(arr[i]);
+        else
+            neg.push_back(arr[i]);
+    }
+
+    // Positives on even indices, negatives on odd.
+    for (int i = 0; i < n / 2; i++)
+    {
+        arr[2 * i] = pos[i];
+        arr[2 * i + 1] = neg[i];
+    }
+    return arr;
+}
+
+int main()
+{
+    vector<int> arr = {1, 2, -4, -5};
+    int n = arr.size();
+    vector<int> ans = rearrange(arr, n);
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << ans[i] << " ";
+    }
+    return 0;
+}
---
+++ 07 - Array Problems/Medium/Rearrange elements by sign/optimal.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> rearrange(vector<int> arr, int n)
+{
+    vector<int> temp(n, 0);
+    int pos = 0, neg = 1;
+
+    for (int i = 0; i < n; i++)
+    {
+        // Fill negative elements in odd indices and inc by 2.
+        if (arr[i] < 0)
+        {
+            temp[neg] = arr[i];
+            neg += 2;
+        }
+        // Fill positive elements in even indices and inc by 2.
+        else
+        {
+            temp[pos] = arr[i];
+            pos += 2;
+        }
+    }
+    return temp;
+}
+
+int main()
+{
+    vector<int> arr = {1, 2, -4, -5};
+    int n = arr.size();
+    vector<int> ans = rearrange(arr, n);
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << ans[i] << " ";
+    }
+    return 0;
+}
