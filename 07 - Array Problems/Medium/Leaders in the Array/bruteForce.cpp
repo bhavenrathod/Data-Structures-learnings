57e4d88bee71fd29cc6c85dcb6cbfed01e9c0009
Bhaven Rathod <rathodbhavendilip@gmail.com>
2024-07-02 00:21:09 +0530

    Leaders in the array done

---
+++ 07 - Array Problems/Medium/Leaders in the Array/bruteForce.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> findLeaders(vector<int> &arr)
+{
+    int n = arr.size();
+    vector<int> ans;
+    for (int i = 0; i < n; i++)
+    {
+        bool flag = true;
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[j] > arr[i])
+            {
+                flag = false;
+                break;
+            }
+        }
+
+        if (flag)
+            ans.push_back(arr[i]);
+    }
+
+    return ans;
+}
+
+int main()
+{
+    vector<int> arr = {10, 22, 3, 0, 6};
+    vector<int> leaders = findLeaders(arr);
+
+    for (auto it : leaders)
+    {
+        cout << it << " ";
+    }
+    return 0;
+}
---
+++ 07 - Array Problems/Medium/Leaders in the Array/optimal.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> findLeaders(vector<int> &arr)
+{
+    vector<int> ans;
+    int last = INT_MIN;
+    int n = arr.size();
+
+    for (int i = n - 1; i >= 0; i--)
+    {
+        if (arr[i] > last)
+        {
+            ans.push_back(arr[i]);
+        }
+        last = max(last, arr[i]);
+    }
+    sort(ans.begin(), ans.end());
+    return ans;
+}
+
+int main()
+{
+    vector<int> arr = {10, 22, 3, 0, 6};
+    vector<int> leaders = findLeaders(arr);
+
+    for (auto it : leaders)
+    {
+        cout << it << " ";
+    }
+    return 0;
+}
